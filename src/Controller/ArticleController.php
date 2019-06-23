9ce7a4b614efbc9d39edd6a6fa88220dedee583a
Ivan Miljancic <ivan.miljancic@gmail.com>
2019-06-23 19:36:29 +0200

    Implemented "Update" form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,17 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/article", name="article.")
+ * @Route(path="/article", name="article.")
  */
 class ArticleController extends AbstractController {
 
 	/**
-	 * @Route("/", name="index");
+	 * @Route(path="/", name="index");
 	 */
 	public function index() {
 
@@ -30,7 +31,10 @@ class ArticleController extends AbstractController {
 
 
 	/**
-	 * @Route("/create", name="create")
+	 * @Route(path="/create", name="create")
+	 * @param Request $request
+	 *
+	 * @return RedirectResponse|Response
 	 */
 	public function create( Request $request ) {
 		$article = new Article();
@@ -62,7 +66,7 @@ class ArticleController extends AbstractController {
 	}
 
 	/**
-	 * @Route("/view/{id}", name="view")
+	 * @Route(path="/view/{id}", name="view")
 	 * @param $id
 	 *
 	 * @return Response
@@ -72,6 +76,41 @@ class ArticleController extends AbstractController {
 
 		return $this->render( 'articles/view.html.twig', [
 			'article' => $article
+		] );
+	}
+
+	/**
+	 * @Route(path="/update/{id}", name="update")
+	 * @param Request $request
+	 * @param $id
+	 *
+	 * @return Response
+	 */
+	public function update( Request $request, $id ) {
+		$article = $this->getDoctrine()->getRepository( Article::class )->find( $id );
+
+		$form = $this->createFormBuilder( $article )
+		             ->add( 'title', TextType::class )
+		             ->add( 'body', TextareaType::class )
+		             ->add( 'save', SubmitType::class, [ 'label' => 'Update' ] )
+		             ->getForm();
+
+		$form->handleRequest( $request );
+
+		if ( $form->isSubmitted() && $form->isValid() ) {
+			$article = $form->getData();
+
+			$em = $this->getDoctrine()->getManager();
+
+			$em->persist( $article );
+			$em->flush();
+
+
+			return $this->redirectToRoute( "article.view", ['id'=>$article->getId()] );
+		}
+
+		return $this->render( 'articles/update.html.twig', [
+			'form' => $form->createView(),
 		] );
 	}
 }
